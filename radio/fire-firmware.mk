89d69920eb66e1529ddcb004ceccf19ea1e32986
YudhoPatrianto <kydh01123@gmail.com>
2025-07-10 21:48:05 +0000

    fire: use fire-firmware to include firmware
    Signed-off-by: YudhoPatrianto <kydh01123@gmail.com>

---
+++ radio/fire-firmware.mk
@@ -0,0 +1,8 @@
+$(call add-radio-file-sha1-checked,gz.img,11466269042119d3fb3df7ee5ff99a6b1b320fd0)
+$(call add-radio-file-sha1-checked,lk.img,d22a2de1fb15ca8b4c4daa05f85b8430320e3302)
+$(call add-radio-file-sha1-checked,md1img.img,bd81aed6f16f6d0c5e64d115ce5b355c3bfe17eb)
+$(call add-radio-file-sha1-checked,preloader_raw.img,490b97eb516fdb64521a9900e6d5a2ae01c44fb0)
+$(call add-radio-file-sha1-checked,scp.img,e7838b385622b21f184831ab5f76b5308858f3d5)
+$(call add-radio-file-sha1-checked,spmfw.img,396ad9d50fd656654cc9d9900bbcb1b75bf88972)
+$(call add-radio-file-sha1-checked,sspm.img,329f3e73c258150232ac1bd2f30157cb14dc84e4)
+$(call add-radio-file-sha1-checked,tee.img,21a10d9e1d331e3a8f0877a06a17ece926f37925)
